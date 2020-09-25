b7942dc4e9c23ac4122ef3805eedb8d1e10a3c04
Anubhav Maheshwari <anubhavmaheshwari0805@gmail.com>
2020-09-25 14:06:11 +0530

    Some improvement of about section

---
+++ Art_Club_Site/js/home.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $.ajax({
+        url: "/doc/about1.txt",
+        dataType: "text",
+        success: function(data) {
+            $("p.about1").text(data);
+        },
+        error: function(error) {
+            alert('error; ' + eval(error));
+        }
+    });
+    $.ajax({
+        url: "/doc/about2.txt",
+        dataType: "text",
+        success: function(data) {
+            $("p.about2").text(data);
+        },
+        error: function(error) {
+            alert('error; ' + eval(error));
+        }
+    });
+    $.ajax({
+        url: "/doc/abou3.txt",
+        dataType: "text",
+        success: function(data) {
+            $("p.about3").text(data);
+        },
+        error: function(error) {
+            alert('error; ' + eval(error));
+        }
+    });
+});
