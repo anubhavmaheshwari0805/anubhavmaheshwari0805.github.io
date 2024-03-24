cb43fde3686c77551736a3b5bd8cfc9c792b244b
Anubhav Maheshwari <55053867+anubhavmaheshwari0805@users.noreply.github.com>
2024-03-24 09:55:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,18 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
     base: "/anubhavmaheshwari0805.github.io",
     plugins: [react()],
-
+    build: {
+        outDir: 'build', // Output directory where optimized files are placed
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    // Ensure JavaScript files have .js extension with correct MIME type
+                    if (assetInfo.name.endsWith('.js')) {
+                        return assetInfo.name;
+                    }
+                    return assetInfo.name;
+                },
+            },
+        },
+    },
 })
